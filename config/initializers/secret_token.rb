3532e1e1fd4a70f6e2450e01f2afb72cbf00bb22
Crystal Spanakos <crystal@spanakos.com>
2013-06-12 15:53:02 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artmore::Application.config.secret_token = '7c5a05428ca57b244f70ad4633f5d08df1f9a46d0e7d32d01a76e9225eaa1ca6cbcc55e0f0af3bb250609c058ea0642930ce816f24aeb067336a30becb076ee3'
