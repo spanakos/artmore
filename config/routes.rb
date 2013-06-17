c9003b494d0a328bc7779372f27ca595e3c9ef50
Crystal Spanakos <crystal@spanakos.com>
2013-06-17 16:14:41 -0400

    Added Devise and Simple_Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Artmore::Application.routes.draw do
   
+  resources :pins
+
+
+  devise_for :users
+
   root :to => 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
