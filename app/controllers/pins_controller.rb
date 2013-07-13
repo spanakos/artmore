8907214197d4c3bdba11673b2dae992c7a332bb2
Crystal Spanakos <crystal@spanakos.com>
2013-07-12 22:58:56 -0400

    Endless Scroll Added

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -5,7 +5,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.order("created_at desc").page(params[:page]).per_page(20)
+    @pins = Pin.order("created_at desc").page(params[:page]).per_page(30)
 
     respond_to do |format|
       format.html # index.html.erb
